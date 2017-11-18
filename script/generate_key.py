aa19d210aef94a5b05dcfabc9f956f9b1b8c3b5e
vsoch <vsochat@stanford.edu>
2017-11-18 07:15:19 -0800

    illegal key punctuation

--- script/generate_key.py
+++ script/generate_key.py
@@ -3,8 +3,8 @@
 import random
 import string
 
-choices = string.ascii_letters + string.digits + string.punctuation
+punctuation = '!#$%&()*+,-./:;<=>?@^_{|}~'
+choices = string.ascii_letters + string.digits + punctuation
 selected = [random.SystemRandom().choice(choices) for _ in range(50)]
 generated_key = ''.join(selected)
-generated_key = generated_key.replace('"','a').replace("'",'B') # remove quotes
 print(generated_key)
