134cc77cee24682ca19884b6b4d9c7643726997e
vsoch <vsochat@stanford.edu>
2017-12-08 08:47:23 -0800

    subparser should print help

--- expfactory/views/utils.py
+++ expfactory/views/utils.py
@@ -62,7 +62,7 @@ def perform_checks(template, do_redirect=False, context=None, next=None):
         return redirect('/')
 
     # Update the user / log
-    app.logger.info("<current:%s><next:%s> for <subid:%s, username:%s>" %(last, next, subid, username))
+    app.logger.info("[current] %s [next] %s for [subid] %s [username] %s" %(last, next, subid, username))
 
     if username is None and app.headless is False:
         flash('You must start a session before doing experiments.')
