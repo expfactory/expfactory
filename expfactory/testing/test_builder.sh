85b066e164f8f9b9d2bce5024f7baa983fad0660
vsoch <vsochat@stanford.edu>
2017-12-14 15:31:51 -0800

    i hate this

--- expfactory/testing/test_builder.sh
+++ expfactory/testing/test_builder.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 echo "Testing builder writing to /tmp"
-mkdir -p /tmp/data
-docker run -v /tmp/data:/data vanessa/expfactory-builder-ci build test-task
+mkdir -p /tmp/data && cd /tmp/data
+docker run -v $PWD:/data vanessa/expfactory-builder-ci build test-task
 
 echo "Building container"
-cd /tmp/data
 docker build -t expfactory/experiments .
