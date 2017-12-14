f646e7452f9d51bed3039d4760d81142376af682
vsoch <vsochat@stanford.edu>
2017-12-14 08:17:09 -0800

    modified:   expfactory/templates/build/docker/builder-ci/build.sh

--- expfactory/templates/build/docker/builder-ci/build.sh
+++ expfactory/templates/build/docker/builder-ci/build.sh
@@ -1,7 +1,7 @@
 #!/use/bin/bash
 
 echo "Building CI builder for testing current PR..."
-cp -r ../../../../. $PWD/
+cp -r ../../../. $PWD/
 cp ../builder/entrypoint.sh $PWD
 ls
 docker build --rm=false -t vanessa/expfactory-builder-ci .
