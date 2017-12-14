030a6baa33705535514e9f256fd85c94c8a311f1
vsoch <vsochat@stanford.edu>
2017-12-14 09:47:21 -0800

    let's try building somewhere else!

--- expfactory/templates/build/docker/builder-ci/build.sh
+++ expfactory/templates/build/docker/builder-ci/build.sh
@@ -1,8 +1,7 @@
 #!/use/bin/bash
 
 echo "Building CI builder for testing current PR..."
-ls ../../../../../
-cp -R ../../../../../. $PWD
-cp ../builder/entrypoint.sh $PWD
-ls
-docker build --rm=false -t vanessa/expfactory-builder-ci .
+mkdir -p /tmp/data
+cp ../builder/entrypoint.sh /tmp/data
+cp Dockerfile /tmp/data
+cd /tmp/data && docker build --rm=false -t vanessa/expfactory-builder-ci .
