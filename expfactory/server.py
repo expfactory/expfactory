a6566ac84d9bfd410b1163ffb7177b6a1abec4a4
Jamie Forth <j.forth@gold.ac.uk>
2018-01-09 10:08:05 +0000

    Add ReverseProxied middleware.
    Flask snippet 35: http://flask.pocoo.org/snippets/35/

--- expfactory/server.py
+++ expfactory/server.py
@@ -142,8 +142,47 @@ EFServer.print_user = print_user
 EFServer.finish_user = finish_user
 EFServer.restart_user = restart_user
 
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+
+        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
+        if server:
+            environ['HTTP_HOST'] = server
+
+        return self.app(environ, start_response)
+
 app = EFServer(__name__)
 app.config.from_object('expfactory.config')
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 # EXPERIMENTS #################################################################
 
