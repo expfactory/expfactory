329848d3cbd71e86dfa4acb7bcbdfc3633696ea8
Vanessa Sochat <vsochat@stanford.edu>
2019-05-15 12:44:38 -0400

    adding curl, and editing builder script
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- expfactory/testing/test_builder.sh
+++ expfactory/testing/test_builder.sh
@@ -3,14 +3,17 @@
 echo "Testing expfactory-builder"
 
 mkdir -p /tmp/data && cd /tmp/data
-docker run -v $PWD:/data vanessa/expfactory-builder-ci build test-task
+docker run -v /tmp/data:/data vanessa/expfactory-builder-ci build test-task
+
+echo "Contents of /tmp/data"
+ls
 
 echo "Building container"
 docker build -t expfactory/experiments .
 
 echo "Start [filesystem][sqlite]"
-docker run -d -v $PWD:/scif/data -p 80:80 --name experiments-fs expfactory/experiments --headless start
-docker run -d -v $PWD:/scif/data --name experiments-sqlite expfactory/experiments --database sqlite --headless start
+docker run -d -v /tmp/data:/scif/data -p 80:80 --name experiments-fs expfactory/experiments --headless start
+docker run -d -v /tmp/data:/scif/data --name experiments-sqlite expfactory/experiments --database sqlite --headless start
 
 # HTTP/1.1 200 OK
 curl -I localhost | grep 200
