6ada7eb263d6994e63ee3e8e9b3d9f89368a1d79
vsoch <vsoch@users.noreply.github.com>
2022-09-27 23:45:18 -0600

    tweak
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- expfactory/database/filesystem.py
+++ expfactory/database/filesystem.py
@@ -196,7 +196,9 @@ def validate_token(self, token):
     if not token.endswith(("finished", "revoked")):
         subid = self.generate_subid(token=token)
         data_base = "%s/%s" % (self.data_base, subid)
+        print("Looking for data base %s" % data_base)
         if not os.path.exists(data_base):
+            print("Data base %s does not exist." % data_base)
             subid = None
     return subid
 
